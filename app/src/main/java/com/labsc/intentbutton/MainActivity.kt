75de357e12374b07674c4df38b87a379d16ef14a
hermantrym <111201811499@mhs.dinus.ac.id>
2022-06-30 13:46:27 +0700

    done

---
+++ app/src/main/java/com/labsc/intentbutton/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.labsc.intentbutton
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        secondActivityBtn.setOnClickListener {
+            val i = Intent(this, SecondActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
